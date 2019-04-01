cf6e8a436f9aca4103766c66bf92550e6830314a
yuezihao <yuezihao@joinquant.com>
2019-04-02 01:18:33 +0800

    creat react form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bulma';
+import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="myform">
+        <div className="title">from</div>
+        <div className="field">
+          <label className="label">Name</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Text input" 
+              value={this.state.name}
+              onChange={e => this.onFieldChange(e)} />
+          </div>
+        </div>
       </div>
     );
+  }
+
+  onFieldChange(e) {
+    const value = e.target.value;
+    this.setState({ name: value });
   }
 }
 
