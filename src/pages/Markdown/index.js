e3e2470eeada3c526bb191a1bb4ef73c695988c0
yuezihao <yuezihao@joinquant.com>
2019-04-10 00:38:19 +0800

    update marked

--- src/pages/Markdown/index.js
+++ src/pages/Markdown/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import marked from 'marked';
 import './style.scss';
 
 const App = () => (
-  <div>
-    Markdown
+  <div className="markdown">
+    <div className="toolbar">
+      <button className="button is-small">
+        <span className="icon is-small">
+          <i className="iconfont icon-editor-bold" />
+        </span>
+      </button>
+      <button className="button is-small">
+        <span className="icon is-small">
+          <i className="iconfont icon-editor-italic" />
+        </span>
+      </button>
+      <button className="button is-small">
+        <span className="icon is-small">
+          <i className="iconfont icon-editor-underline" />
+        </span>
+      </button>
+      <button className="button is-small">
+        <span className="icon is-small">
+          <i className="iconfont icon-editor-list-bulleted" />
+        </span>
+      </button>
+    </div>
+    <div className="body box">
+      <div className="editor">
+        <textarea className="textarea" />
+      </div>
+      <div className="preview">
+      </div>
+    </div>
   </div>
 )
 
