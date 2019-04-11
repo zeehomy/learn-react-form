900cf35dd0832f7d9f3d3085fd652d906aad1713
yuezihao <yuezihao@joinquant.com>
2019-04-11 13:55:16 +0800

    update markdown

--- src/pages/Markdown/index.js
+++ src/pages/Markdown/index.js
@@ -1,39 +1,57 @@
 import React from 'react';
 import marked from 'marked';
+import 'github-markdown-css';
 import './style.scss';
 
-const Markdown = () => (
-  <div className="markdown">
-    <div className="toolbar">
-      <button className="button is-small">
-        <span className="icon is-small">
-          <i className="iconfont icon-editor-bold" />
-        </span>
-      </button>
-      <button className="button is-small">
-        <span className="icon is-small">
-          <i className="iconfont icon-editor-italic" />
-        </span>
-      </button>
-      <button className="button is-small">
-        <span className="icon is-small">
-          <i className="iconfont icon-editor-underline" />
-        </span>
-      </button>
-      <button className="button is-small">
-        <span className="icon is-small">
-          <i className="iconfont icon-editor-list-bulleted" />
-        </span>
-      </button>
-    </div>
-    <div className="body box">
-      <div className="editor">
-        <textarea className="textarea" />
+class Markdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: ''
+    };
+  }
+
+  render() {
+    const html = marked(this.state.markdown);
+    return (
+      <div className="markdown">
+        <div className="toolbar">
+          <button className="button is-small">
+            <span className="icon is-small">
+              <i className="iconfont icon-editor-bold" />
+            </span>
+          </button>
+          <button className="button is-small">
+            <span className="icon is-small">
+              <i className="iconfont icon-editor-italic" />
+            </span>
+          </button>
+          <button className="button is-small">
+            <span className="icon is-small">
+              <i className="iconfont icon-editor-underline" />
+            </span>
+          </button>
+          <button className="button is-small">
+            <span className="icon is-small">
+              <i className="iconfont icon-editor-list-bulleted" />
+            </span>
+          </button>
+        </div>
+        <div className="body box">
+          <div className="editor">
+            <textarea className="textarea" value={this.state.markdown} 
+              onChange={e => this.onFieldChange('markdown', e)} />
+          </div>
+          <div className="preview markdown-body" dangerouslySetInnerHTML={{ __html: html }}>
+          </div>
+        </div>
       </div>
-      <div className="preview">
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+
+  onFieldChange(name, e) {
+    this.setState({ [name]: e.target.value });
+  }
+}
 
 export default Markdown;
